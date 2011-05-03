7a9a8f44d6a7060198499999337f46930732fe82
Riot M <minimalisticriot@yahoo.com>
2011-05-03 12:19:55 +0800

    script.js: fix function unfocusClick()

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,8 @@
 /* hide dotted outline on the clicked anchor by unfocusing it
  * http://haslayout.net/css-tuts/Removing-Dotted-Border-on-Clicked-Links */
-function unfocusClick() {
-  var links = document.getElementById('header').getElementsByTagName('a');
+function unfocusClick(id) {
+  if (id == null) return;
+  var links = document.getElementById(id).getElementsByTagName('a');
   for ( var i = 0; i < links.length; i++ ) {
     links[i].onmousedown = function () {
       this.blur();
